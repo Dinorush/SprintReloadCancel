8ba0b950bf3d76a9ed76aca669dbb0153e53843a
Dinorush <onparnorthstar@gmail.com>
2024-07-08 22:31:19 -0400

    Version bump

--- SprintReloadCancel/EntryPoint.cs
+++ SprintReloadCancel/EntryPoint.cs
@@ -4,7 +4,7 @@ using HarmonyLib;
 
 namespace SprintReloadCancel
 {
-    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.3.1")]
+    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.3.2")]
     [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
     internal sealed class EntryPoint : BasePlugin
     {
